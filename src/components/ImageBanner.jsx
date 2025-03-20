7afe1e92fc60bb434eac0c2d3ea1186ac48afcb1
ABS3D <abbasyoussouf2@gmail.com>
2025-03-21 00:51:18 +0100

    Correction du carrousel : suppression des flèches si une seule image

--- src/components/ImageBanner.jsx
+++ src/components/ImageBanner.jsx
@@ -40,7 +40,7 @@ export function ImageBanner(props) {
   return (
     <div className="image__banner">
       <div className="image__container">{getCarouselOrDefaultImage()}</div>
-      {arePicturesAvailable() && (
+      {arePicturesAvailable() && pictures.length > 1 && (
         <>
           <button className="btn btn-previous" onClick={moveToPrevious}>
             <i className="fas fa-chevron-left"></i>
